331abfa612102502a39a62e7f58bc8a3ebcc7b76
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-02-24 02:41:04 +0800

    test code for str built-in methods

--- tests/test.py
+++ tests/test.py
@@ -18,3 +18,18 @@ def test_dict_get():
 def test_complex_num():
     assert 1j * 2j == -2
     assert (3 + 4j) * (2 + 3j) == (-6 + 17j)
+
+
+def test_str_methods():
+    assert "level".count('l') == 2
+    assert "rich".endswith("ch")
+    assert "level".find('e') == 1
+    assert "1 + 2 = {0}".format(3) == "1 + 2 = {a}".format(a='3') == "1 + 2 = 3"
+    assert "apple".isalpha()
+    assert not "index1".isalpha()
+    assert "3456".isnumeric()
+    assert "black".upper() == "BLACK"
+    assert "this".replace('is', 'at') == "that"
+    assert "a,b".split(',') == ['a', 'b']
+    assert "idea".startswith("id")
+    assert "    inside    ".strip() == "inside"
